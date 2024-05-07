685cbcfe7f44bdb964c2214656b71c856b649dee
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-05-07 15:34:19 +0300

    feat: add the controller of detailProduct

--- src/main/java/com/gestion/gestionstation/Controller/DetailProductController.java
+++ src/main/java/com/gestion/gestionstation/Controller/DetailProductController.java
@@ -1,2 +1,23 @@
-package com.gestion.gestionstation.Controller;public class DetailProductController {
+package com.gestion.gestionstation.Controller;
+
+import com.gestion.gestionstation.Model.DetailProduct;
+import com.gestion.gestionstation.Service.DetailProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+public class DetailProductController {
+
+private DetailProductService detailProductService;
+
+    public DetailProductController(DetailProductService detailProductService) {
+        this.detailProductService = detailProductService;
+    }
+    @GetMapping("/product")
+    public List<DetailProduct> findAll() throws SQLException, ClassNotFoundException {
+        return detailProductService.findAll();
+    }
 }
