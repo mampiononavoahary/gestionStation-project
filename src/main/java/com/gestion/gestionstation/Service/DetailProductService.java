064a8cb6d9a3061186bb9b41c59ef95901d2111c
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-05-07 15:33:48 +0300

    feat: add the service of detaitProduct

--- src/main/java/com/gestion/gestionstation/Service/DetailProductService.java
+++ src/main/java/com/gestion/gestionstation/Service/DetailProductService.java
@@ -1,2 +1,17 @@
-package com.gestion.gestionstation.Service;public class DetailProductService {
+package com.gestion.gestionstation.Service;
+
+import com.gestion.gestionstation.Model.DetailProduct;
+import com.gestion.gestionstation.Repository.DetailProductCrudOperation;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class DetailProductService {
+    DetailProductCrudOperation detailProductCrudOperation = new DetailProductCrudOperation();
+
+    public List<DetailProduct> findAll() throws SQLException, ClassNotFoundException {
+        return detailProductCrudOperation.findAll();
+    }
 }
